ca4be295b15a2e3b1e7265c0aad9d631338ccd15
Shrav211 <73655596+Shrav211@users.noreply.github.com>
2020-11-26 00:55:03 +0530

    Add files via upload

---
+++ secong.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+   int shravan[10];
+
+   cout << "Element - Value " << endl;
+
+   for(int x = 0; x<9; x++){
+        shravan[x] = 99;
+
+    cout << x << " ---------- " << shravan[x] << endl;
+   }
+}
